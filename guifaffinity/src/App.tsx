49d29b9091d1edd4744fac3667c424573cfdbf9c
Oleg <oleg.pafik@eversens.com>
2023-06-02 17:47:40 +0200

    feat(app): Add gifs with https directly

--- guifaffinity/src/App.tsx
+++ guifaffinity/src/App.tsx
@@ -18,9 +18,13 @@ function App() {
             <h2>Los guif más trendings del momento</h2>
           </div>
           <div className='panel__gifs'>
-            <img data-testid='gif' className='gif' src='assets/Gif1.svg' alt='gif not found'/>
-            <img className='gif' src='assets/Gif1.svg' alt='gif not found'/>
-            <img className='gif' src='assets/Gif1.svg' alt='gif not found'/>
+            <img data-testid='gif' className='gif' src='https://media.giphy.com/media/xT77XZrTKOxycjaYvK/giphy.gif' alt='gif not found'/>
+            <img className='gif' src='https://media.giphy.com/media/KTnBZB1CsqMOQ/giphy.gif' alt='gif not found'/>
+            <img className='gif' src='https://media.giphy.com/media/2QYXlNR3pw0OtNC9Ex/giphy.gif' alt='gif not found'/>
+            <img className='gif' src='https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExYWU0MzNlODQ3NmU1MTljYTg0Zjk2YTU2ZjRkNmJiODczODBmYzkzOSZlcD12MV9pbnRlcm5hbF9naWZzX2dpZklkJmN0PWc/YmHk04qjJm4kiGwZWJ/giphy.gif' alt='gif not found'/>
+            <img className='gif' src='https://media.giphy.com/media/1uPiL9Amv5zkk/giphy.gif' alt='gif not found'/>
+            <img className='gif' src='https://media.giphy.com/media/KTnBZB1CsqMOQ/giphy.gif' alt='gif not found'/>
+            <img className='gif' src='https://media.giphy.com/media/2QYXlNR3pw0OtNC9Ex/giphy.gif' alt='gif not found'/>
           </div>
         </div>
       </section>
