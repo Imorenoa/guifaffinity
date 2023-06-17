e675801f12f36844f4d847734d3d637b5ddc532a
Igor <moreno.igor.87@gmail.com>
2023-06-17 12:39:59 +0200

    style(guifaffinity-back):rename endpoint

--- guifaffinity-back/src/routes.ts
+++ guifaffinity-back/src/routes.ts
@@ -2,11 +2,11 @@ import { Router } from "express";
 
 const routes = Router();
 
-// GET /api/memes
-routes.get('/memes', (req, res, next) => {
+// GET /api/gifs
+routes.get("/gifs", (req, res, next) => {
   const db = req.context.db;
-  const memes = db.get("memes").take(50).value();
-  res.status(200).json(memes);
-  })
+  const gifs = db.get("memes").take(50).value();
+  res.status(200).json(gifs);
+});
 
 export default routes;
