6bff230bae698b32f83cacb4c8d9d4b4f91797df
Oleg <oleg.pafik@eversens.com>
2023-06-20 17:53:29 +0200

    feat(routes): Return gifs filtered by tag

--- guifaffinity-back/src/routes.ts
+++ guifaffinity-back/src/routes.ts
@@ -5,34 +5,16 @@ import { Meme } from "./DatabaseSchema";
 const routes = Router();
 
 const filterGifsByTag = (databaseGifs: Meme[], tag: string) => {
-  return databaseGifs.filter((gif) => {
-    gif.tags.includes(`#${tag}`);
-  });
+  return databaseGifs.filter((gif) => gif.tags.includes(`#${tag}`));
 };
-
-// GET /api/gifs
-routes.get("/gifs", (req, res, next) => {
-  const tag = req.query.tag as string;
-  const db = req.context.db;
-  const gifsDatabase = db.get("memes").take(50).value();
-  if (tag) {
-    returnGifsByTag(gifsDatabase, tag, res);
-    return;
-  }
-
-  const gifs: Gif[] = mapGifs(gifsDatabase);
-  res.status(200).json(gifs);
-});
 
 const returnGifsByTag = (gifs: Meme[], tag: string, res: Response) => {
   const gifsByTag = filterGifsByTag(gifs, tag);
-
-  const mappedGifs: Gif[] = mapGifs(gifsByTag);
-
-  res.status(200).json(mappedGifs);
+  const gifsClient: Gif[] = convertToGifsClient(gifsByTag);
+  res.status(200).json(gifsClient);
 };
 
-const mapGifs = (gifs: Meme[]) => {
+const convertToGifsClient = (gifs: Meme[]) => {
   return gifs.map((gif) => ({
     id: gif.id,
     src: gif.images.original.url,
@@ -42,5 +24,18 @@ const mapGifs = (gifs: Meme[]) => {
     tags: gif.tags,
   }));
 };
+
+// GET /api/gifs
+routes.get("/gifs", (req, res, next) => {
+  const tag = req.query.tag as string;
+  const db = req.context.db;
+  const gifsDatabase = db.get("memes").take(50).value()
+  if (tag) {
+    returnGifsByTag(gifsDatabase, tag, res);
+    return;
+  }
+  const gifsClient: Gif[] = convertToGifsClient(gifsDatabase);
+  res.status(200).json(gifsClient);
+});
 
 export default routes;
