19aa42443baf9bf298100444cb604b966e3d3af5
Oleg <oleg.pafik@eversens.com>
2023-06-30 17:10:53 +0200

    refactor(header.test.tsx): Render only header component, not the whole app

--- guifaffinity-front/src/components/header/header.test.tsx
+++ guifaffinity-front/src/components/header/header.test.tsx
@@ -1,8 +1,8 @@
 import { render, screen } from "@testing-library/react";
-import App from "../../app/App";
+import { Header } from "./header";
 
 test("display logo", () => {
-  render(<App />);
+  render(<Header />);
   const screenedLogo = screen.getByAltText("Logo Guifaffinity");
 
   expect(screenedLogo).toBeVisible();
